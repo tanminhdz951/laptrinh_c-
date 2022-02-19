41b50e752141ac7c4d026a16617358183b21ab29
Your Name <you@example.com>
2022-02-19 11:17:03 +0700

    up file

---
+++ code_theo_mau_bai4/code_theo_mau_bai4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace code_theo_mau_bai4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
